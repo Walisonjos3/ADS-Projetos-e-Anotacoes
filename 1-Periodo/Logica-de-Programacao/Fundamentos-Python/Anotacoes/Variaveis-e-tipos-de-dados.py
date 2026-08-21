d658f414832a207fe3422572a7ba90559481824a
Walison José <walisonjose128@gmail.com>
2026-08-21 20:08:01 -0300

    Create Variaveis-e-tipos-de-dados.py
    Adicionei documentação sobre tipos de dados e variáveis na linguagem Python.

---
+++ 1-Periodo/Logica-de-Programacao/Fundamentos-Python/Anotacoes/Variaveis-e-tipos-de-dados.py
@@ -0,0 +1,13 @@
+print("--- 2. VARIÁVEIS E TIPOS PRIMITIVOS ---")
+    
+    # Declarando variáveis
+    nome: str = "Walison"      # String
+    idade: int = 18            # Inteiro
+    coeficiente: float = 8.75  # Decimal (Float)
+    matriculado: bool = True   # Booleano
+    
+    # Verificando tipos com a função type()
+    print(f"Variável 'nome' ({nome}): {type(nome)}")
+    print(f"Variável 'idade' ({idade}): {type(idade)}")
+    print(f"Variável 'coeficiente' ({coeficiente}): {type(coeficiente)}")
+    print(f"Variável 'matriculado' ({matriculado}): {type(matriculado)}")
