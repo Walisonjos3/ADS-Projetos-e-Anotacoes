5184965e05479f01cf610ee8542c8b8c4a9a1e39
Walison José <walisonjose128@gmail.com>
2026-08-23 00:23:47 -0300

    Colecoes-Listas-tuplas-Dicionarios.py
    Adicionei documentação sobre listas, tuplas e dicionários na linguagem Python.

---
+++ 1-Periodo/Logica-de-Programacao/Fundamentos-Python/Anotacoes/Colecoes-Listas-tuplas-Dicionarios.py
@@ -0,0 +1,20 @@
+print("--- 6. COLEÇÕES (ESTRUTURAS DE DADOS) ---")
+    
+    # Lista (Mutável)
+    linguagens = ["Python", "C", "Java"]
+    linguagens.append("SQL")
+    print(f"Lista de Linguagens: {linguagens}")
+    print(f"Primeiro elemento: {linguagens[0]}")
+    
+    # Tupla (Imutável)
+    coordenadas = (-6.98, -37.27)
+    print(f"Tupla de Coordenadas: {coordenadas}")
+    
+    # Dicionário (Chave-Valor)
+    aluno = {
+        "nome": "Walison",
+        "curso": "ADS",
+        "semestre": 2
+    }
+    print(f"Dicionário Aluno: {aluno}")
+    print(f"Valor da chave 'curso': {aluno['curso']}")
