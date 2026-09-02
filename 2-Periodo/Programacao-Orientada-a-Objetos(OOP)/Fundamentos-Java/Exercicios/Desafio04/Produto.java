47bdc2c70612144814c7226d403f3db3cc4224f9
Walison José <walisonjose128@gmail.com>
2026-09-02 19:40:04 -0300

    Atualizei o exercício

--- 2-Periodo/Programacao-Orientada-a-Objetos(OOP)/Fundamentos-Java/Exercicios/Desafio04/Produto.java
+++ 2-Periodo/Programacao-Orientada-a-Objetos(OOP)/Fundamentos-Java/Exercicios/Desafio04/Produto.java
@@ -6,17 +6,24 @@ public class Produto {
 
     public Produto(String nome, double preco, int quantidade) {
         this.nome = nome;
-        this.preco = preco;
-        this.quantidade = quantidade;
+        setPreco(preco);
+        setQuantidade(quantidade);
     }
 
-
     public void valorTotalEstoque() {
-        System.out.printf("Valor total em estoque: R$ %.2f", this.getPreco() * this.getQuantidade());
+        System.out.println("====================================================");
+        System.out.printf("Valor total em estoque de %s: R$ %.2f%n",this.getNome(), this.getPreco() * this.getQuantidade());
+        System.out.println("====================================================");
     }
 
     public void aplicarDesconto(double desconto) {
-        this.preco -= desconto;
+        if (this.preco <= 0){
+            System.out.println("\nPreço insuficiente para desconto");
+        } else {
+            double valorDesconto = this.preco * (desconto / 100);
+            this.preco -= valorDesconto;
+            System.out.printf("R$ %.2f de desconto aplicado!", desconto);
+        }
     }
 
     public void mostrarDados() {
@@ -34,7 +41,23 @@ public class Produto {
         return this.preco;
     }
 
+    private void setPreco(double preco) {
+        if (preco <= 0) {
+            System.out.println("Preço inválido!");
+        } else {
+            this.preco = preco;
+        }
+    }
+
     public int getQuantidade() {
         return this.quantidade;
+    }
+
+    private void setQuantidade(int quantidade) {
+        if (quantidade <= 0) {
+            System.out.println("Quantidade inválida!");
+        } else {
+            this.quantidade = quantidade;
+        }
     }
 }
