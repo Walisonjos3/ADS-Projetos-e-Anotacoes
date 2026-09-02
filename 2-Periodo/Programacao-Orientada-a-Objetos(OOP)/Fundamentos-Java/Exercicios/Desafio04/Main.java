43fde83304a8650938c78b8a8a157d8e094a6410
Walison José <walisonjose128@gmail.com>
2026-09-02 19:39:30 -0300

    Atualizei o exercicios

--- 2-Periodo/Programacao-Orientada-a-Objetos(OOP)/Fundamentos-Java/Exercicios/Desafio04/Main.java
+++ 2-Periodo/Programacao-Orientada-a-Objetos(OOP)/Fundamentos-Java/Exercicios/Desafio04/Main.java
@@ -2,16 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Produto produto1 = new Produto("Caneca", 15.00, 5);
+        Produto produto1 = new Produto("Caneca", 100, 5);
         Produto produto2 = new Produto("Tênis", 120.00, 4);
 
         produto1.mostrarDados();
         produto1.aplicarDesconto(5);
         produto1.mostrarDados();
+        produto1.valorTotalEstoque();
 
         produto2.mostrarDados();
-
-
+        produto2.valorTotalEstoque();
     }
 
 }
