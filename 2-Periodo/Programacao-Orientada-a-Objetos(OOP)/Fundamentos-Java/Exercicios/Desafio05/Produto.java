47d9537e86271b92d9ee1f6307259295dcc1e9c6
Walison José <walisonjose128@gmail.com>
2026-09-09 19:37:25 -0300

    Add Produto de sistema de vendas

---
+++ 2-Periodo/Programacao-Orientada-a-Objetos(OOP)/Fundamentos-Java/Exercicios/Desafio05/Produto.java
@@ -0,0 +1,80 @@
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        setPreco(preco);
+        setQuantidade(quantidade);
+    }
+
+    public void valorTotalEstoque() {
+        System.out.println("====================================================");
+        System.out.printf("Valor total em estoque de %s: R$ %.2f%n",this.getNome(), this.getPreco() * this.getQuantidade());
+        System.out.println("====================================================");
+    }
+
+    public void aplicarDesconto(double desconto) {
+        if (this.preco <= 0){
+            System.out.println("\nPreço insuficiente para desconto");
+        } else {
+            double valorDesconto = this.preco * (desconto / 100);
+            this.preco -= valorDesconto;
+            System.out.printf("\nR$ %.2f de desconto aplicado!", desconto);
+        }
+    }
+
+    public void retirarEstoque(int quantidade) {
+        if (quantidade > this.getQuantidade()){
+            System.out.println("\nImpossível diminuir o estoque.");
+        } else {
+            this.setQuantidade(this.getQuantidade() - quantidade);
+            System.out.println("\nProdutos retirados.");
+        }
+    }
+
+    public void temEstoque(int quantidade) {
+        if (quantidade > this.getQuantidade()) {
+            System.out.println("\nNão tem essa quantidade no estoque.");
+        } else {
+            System.out.println("\nTem essa quantidade no estoque.");
+        }
+    }
+
+    public void mostrarDados() {
+        System.out.println("\n  <<  Mostrando Produto  >>");
+        System.out.printf("Nome do produto: %s%n", this.getNome());
+        System.out.printf("Preço do produto: R$ %.2f%n", this.getPreco());
+        System.out.printf("Quantidade em estoque: %d unidades%n", this.getQuantidade());
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    private void setPreco(double preco) {
+        if (preco <= 0) {
+            System.out.println("Preço inválido!");
+        } else {
+            this.preco = preco;
+        }
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    private void setQuantidade(int quantidade) {
+        if (quantidade <= 0) {
+            System.out.println("Quantidade inválida!");
+        } else {
+            this.quantidade = quantidade;
+        }
+    }
+}
